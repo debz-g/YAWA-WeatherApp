862894a1d541426f325cd939d0ad2094821cd363
debz-g <debz.exe@gmail.com>
2021-11-29 00:44:40 +0530

    ui changes

--- app/src/main/java/dev/redfox/yawa/SplashScreen.kt
+++ app/src/main/java/dev/redfox/yawa/SplashScreen.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.android.gms.location.FusedLocationProviderClient
+import dev.redfox.yawa.databinding.ActivitySplashScreenBinding
+
+private lateinit var binding: ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    lateinit var mfusedlocation:FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-        supportActionBar?.hide()
+        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Handler().postDelayed(Runnable {
 
            startActivity(Intent(this,MainActivity::class.java))
             finish()
-        },600)
+        },400)
     }
 }
