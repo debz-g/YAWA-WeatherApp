f766fe6532fef1d2ebb7421ff192ac4f2081ed3a
debz-g <debz.exe@gmail.com>
2021-11-29 00:55:40 +0530

    location perms 1

--- app/src/main/java/dev/redfox/yawa/SplashScreen.kt
+++ app/src/main/java/dev/redfox/yawa/SplashScreen.kt
@@ -1,24 +1,55 @@
 package dev.redfox.yawa
 
 import android.content.Intent
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dev.redfox.yawa.databinding.ActivitySplashScreenBinding
 
 private lateinit var binding: ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
-    lateinit var mfusedlocation:FusedLocationProviderClient
+    lateinit var mfusedlocation: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+
         Handler().postDelayed(Runnable {
 
-           startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
-        },400)
+        }, 400)
+    }
+
+    private fun getLastLocation() {
+        if (checkPermission()) {
+            if(locationEnable()){
+                mfusedlocation.lastLocation.addOnCompleteListener {
+                    task->
+                    var location:Location?=task.result
+                }
+            }
+        }
+        else
+            requestPermission()
+    }
+
+    private fun locationEnable(): Boolean {
+
+    }
+
+    private fun requestPermission() {
+
+    }
+
+    private fun checkPermission(): Boolean{
+
     }
 }
