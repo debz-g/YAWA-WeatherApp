6090bbb1e96ad79d2638fe99330c0f59e0802a23
Madhur Mehta <12madhurmehta@gmail.com>
2021-11-28 23:47:20 +0530

    Added Splash Screen and added view binding Added different permissions

--- app/src/main/java/dev/redfox/yawa/MainActivity.kt
+++ app/src/main/java/dev/redfox/yawa/MainActivity.kt
@@ -2,10 +2,14 @@ package dev.redfox.yawa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.redfox.yawa.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
     }
 }
