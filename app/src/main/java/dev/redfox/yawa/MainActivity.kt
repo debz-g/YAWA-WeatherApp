0c0815425db23784d0ac15042ff70f5537386877
debz-g <debz.exe@gmail.com>
2021-11-28 14:04:36 +0530

    Initial commit

---
+++ app/src/main/java/dev/redfox/yawa/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.redfox.yawa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
