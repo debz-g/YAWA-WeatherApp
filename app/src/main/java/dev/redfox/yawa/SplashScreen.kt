618f38e60a9ebb391a9688afe5fc1da52b61e67a
debz-g <debz.exe@gmail.com>
2021-11-29 18:46:52 +0530

    location perm update 2

--- app/src/main/java/dev/redfox/yawa/SplashScreen.kt
+++ app/src/main/java/dev/redfox/yawa/SplashScreen.kt
@@ -6,12 +6,15 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
@@ -21,21 +24,21 @@ import dev.redfox.yawa.databinding.ActivitySplashScreenBinding
 private lateinit var binding: ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
-    lateinit var mfusedlocation: FusedLocationProviderClient
-    private var reqCode = 1010
+//    lateinit var mfusedlocation: FusedLocationProviderClient
+//    private var reqCode = 1010
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
+//        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+//        getLastLocation()
 
-//        Handler().postDelayed(Runnable {
-//
-//            startActivity(Intent(this, MainActivity::class.java))
-//            finish()
-//        }, 1000)
+        Handler().postDelayed(Runnable {
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 400)
     }
 
     /*
@@ -47,90 +50,82 @@ class SplashScreen : AppCompatActivity() {
 
     */
 
-    private fun getLastLocation() {
-        if (checkPermission()) {
-            if (locationEnable()) {
-                mfusedlocation.lastLocation.addOnCompleteListener { task ->
-                    var location: Location? = task.result
-                    if (location == null) {
-                        newLocation()
-                    } else {
-                        Handler(Looper.getMainLooper()).postDelayed({
-                            val intent = Intent(this, MainActivity::class.java)
-                            intent.putExtra("latitide", location.latitude.toString())
-                            intent.putExtra("longitide", location.longitude.toString())
-                            startActivity(intent)
-                            finish()
-                        }, 1500)
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Please Turn on GPS from settings", Toast.LENGTH_SHORT).show()
-            }
-        } else
-            requestPermission()
+//    private fun getLastLocation() {
+//        if (checkPermission()) {
+//            if (locationEnable()) {
+//                mfusedlocation.lastLocation.addOnCompleteListener { task ->
+//                    var location: Location? = task.result
+//                    if (location == null) {
+//                        newLocation()
+//                    } else {
+//                        Log.i("Location", location.longitude.toString())
+//                    }
+//                }
+//            } else {
+//                Toast.makeText(this, "Please Turn on GPS from settings", Toast.LENGTH_SHORT).show()
+//            }
+//        } else
+//            requestPermission()
+//    }
+//
+//    @SuppressLint("MissingPermission")
+//    private fun newLocation() {
+//        var locationReq=com.google.android.gms.location.LocationRequest()
+//        locationReq.priority=com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+//        locationReq.interval=0
+//        locationReq.fastestInterval=0
+//        locationReq.numUpdates=1
+//        mfusedlocation=LocationServices.getFusedLocationProviderClient(this)
+//        mfusedlocation.requestLocationUpdates(locationReq,locationCallBack, Looper.myLooper())
+//    }
+//
+//    private val locationCallBack = object:LocationCallback(){
+//        override fun onLocationResult(p0: LocationResult) {
+//            var lastLocation:Location=p0.lastLocation
+//        }
+//    }
+//
+//    private fun locationEnable(): Boolean {
+//        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+//            LocationManager.NETWORK_PROVIDER
+//        )
+//    }
+//
+//    private fun requestPermission() {
+//        ActivityCompat.requestPermissions(
+//            this, arrayOf(
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION
+//            ), reqCode
+//        )
+//    }
+//
+//    private fun checkPermission(): Boolean {
+//        if (
+//            ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED ||
+//            ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    @SuppressLint("MissingSuperCall")
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        if (requestCode == reqCode) {
+//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                getLastLocation()
+//            }
+//        }
     }
-
-    @SuppressLint("MissingPermission")
-    private fun newLocation() {
-        var locationReq = com.google.android.gms.location.LocationRequest()
-        locationReq.priority =
-            com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationReq.interval = 0
-        locationReq.fastestInterval = 0
-        locationReq.numUpdates = 1
-        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
-        mfusedlocation.requestLocationUpdates(locationReq, locationCallBack, Looper.myLooper())
-    }
-
-    private val locationCallBack = object : LocationCallback() {
-        override fun onLocationResult(p0: LocationResult) {
-            var lastLocation: Location = p0.lastLocation
-        }
-    }
-
-    private fun locationEnable(): Boolean {
-        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ), reqCode
-        )
-    }
-
-    private fun checkPermission(): Boolean {
-        if (
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    @SuppressLint("MissingSuperCall")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == reqCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation()
-            }
-        }
-    }
-}
